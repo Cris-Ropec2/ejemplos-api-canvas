c554195577102056ace5cfc5db0dde4911a71b3a
Cris-Ropec2 <l23200892@pachuca.tecnm.mx>
2026-02-18 16:29:16 -0600

    se agrega Boostrap

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+function resizeCanvas(canvas, container) {
+  const rect = container.getBoundingClientRect();
+  canvas.width = Math.floor(rect.width);
+  canvas.height = Math.floor(rect.height);
+}
+
+function draw() {
+  const canvas = document.getElementById("canvas");
+  const container = canvas.parentElement;
+
+  if (!canvas.getContext) return;
+
+  resizeCanvas(canvas, container);
+
+  const ctx = canvas.getContext("2d");
+
+  // Limpia todo
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // =============================
+  // RECTÁNGULO RELLENO
+  // =============================
+  ctx.fillStyle = "#2dd4bf";
+  ctx.fillRect(25, 25, 100, 100);
+
+  // =============================
+  // LIMPIEZA INTERNA
+  // =============================
+  ctx.clearRect(45, 45, 60, 60);
+
+  // =============================
+  // CONTORNO
+  // =============================
+  ctx.strokeStyle = "#111827";
+  ctx.lineWidth = 3;
+  ctx.strokeRect(50, 50, 50, 50);
+
+  // =============================
+  // TRIÁNGULO (Path API)
+  // =============================
+  ctx.beginPath();
+  ctx.moveTo(200, 100);
+  ctx.lineTo(250, 150);
+  ctx.lineTo(250, 50);
+  ctx.closePath();
+
+  ctx.fillStyle = "#f97316";
+  ctx.fill();
+}
+
+function initUI() {
+  document.getElementById("year").textContent =
+    new Date().getFullYear();
+
+  document
+    .getElementById("btnRedraw")
+    .addEventListener("click", draw);
+
+  window.addEventListener("resize", draw);
+
+  draw();
+}
+
+initUI();
